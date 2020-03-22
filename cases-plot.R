6815fb6ad6c254120ed452877cc92f6b2a19fe1a
Jorick <roels.jorick@gmail.com>
2020-03-22 12:49:35 +0100

    graphs start form day 1 of 100 cases

--- cases-plot.R
+++ cases-plot.R
@@ -14,14 +14,28 @@ source("data-loader.R")
 # read data
 cov_data <- read_csv("procdata/data.csv")
 
+#------------------------
+# Data transformation
 
-# Make plots
 # Cases starting from 100 confirmed cases
 cov_data_100 <- cov_data %>% 
   filter(confirmed >= 100)
 
+# Start date of eacht country for 100 confirmed cases
+start_dates_all <- cov_data_100 %>% 
+  group_by(Country) %>% 
+  summarise(start_date = min(date))
+
+# Add days column to each country starting the count from each start date
+cov_data_100 <- cov_data_100 %>% 
+  left_join(start_dates_all) %>% 
+  mutate(day = as.numeric(date - (start_date - 1))) %>% 
+  select(-start_date)
+
+# -----------------------------
+# Make the plots
 log_confirmed <- cov_data_100 %>% 
-  ggplot(mapping = aes(date, confirmed)) +
+  ggplot(mapping = aes(day, confirmed)) +
   geom_line(mapping = aes(color = Country)) +
   geom_point(mapping = aes(color = Country)) +
   scale_y_log10() +
@@ -29,27 +43,26 @@ log_confirmed <- cov_data_100 %>%
   ylab("log(confirmed cases)")
 
 linear_deaths <- cov_data_100 %>% 
-  ggplot(mapping = aes(date, deaths)) +
+  ggplot(mapping = aes(day, deaths)) +
   geom_line(mapping = aes(color = Country)) +
   geom_point(mapping = aes(color = Country)) +
   ggtitle("Number of deaths in 8 countries")
 
 log_deaths <- cov_data_100 %>% 
-  ggplot(mapping = aes(date, deaths)) +
+  ggplot(mapping = aes(day, deaths)) +
   geom_line(mapping = aes(color = Country)) +
   geom_point(mapping = aes(color = Country)) +
-  scale_y_log10()
+  scale_y_log10() +
+  ggtitle("Number of deaths in 8 countries") +
+  ylab("Log(deaths)")
 
 #------------------
-# Modeling only Belgium
+# Simple model for Belgium
 data_belgium <- cov_data_100 %>%
   filter(Country == "Belgium")
 
-startdate <- min(data_belgium$date) - 1
-
 data_belgium <- data_belgium %>% 
-  mutate(day = as.numeric(date - startdate), 
-         lconfirmed = log(confirmed))
+  mutate(lconfirmed = log(confirmed))
 
 model <- lm(lconfirmed ~ day, data = data_belgium)
 
@@ -70,7 +83,7 @@ belgium_plot <- belgium_pred %>%
 # Plot everything
 
 (belgium_plot) /
-  (log_confirmed | linear_deaths) +
+  (log_confirmed | log_deaths) +
   plot_layout(guides = 'collect') +
   plot_annotation(caption = paste("Data obtained from Johns Hopkins CSSE:",
                                   "https://github.com/CSSEGISandData/COVID-19")) &
